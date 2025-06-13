71e778f60a376c97cd985a12980c49c06ba16fe3
Mengjie <e1285298@u.nus.edu>
2025-06-13 14:15:07 +0800

    layout reset

--- app.py
+++ app.py
@@ -7,6 +7,8 @@
 
 import streamlit as st
 
+st.set_page_config(page_title="ML + Optimizer Visualizer", layout="wide")
+
 import pandas as pd
 import numpy as np
 
@@ -59,6 +61,23 @@ llm = ChatOpenAI(
 )
 
 memory = ConversationBufferMemory()
+
+st.markdown("""
+    <style>
+    .stMarkdown h4 {
+        margin-bottom: 0.2em;
+    }
+    .stRadio > div {
+        margin-top: 0;
+        margin-bottom: 0.5em;
+    }
+    .stSelectbox, .stTextInput, .stSlider {
+        padding-top: 0;
+        padding-bottom: 0;
+        margin-bottom: 0.3em;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
 # === UI Config ===
 # st.set_page_config(page_title="ML + Optimizer Visualizer", layout="wide")
@@ -1172,8 +1191,8 @@ elif mode == "🌋 Optimization Playground":
         col_sidebar, col_main = st.columns([1, 3])
 
         with col_sidebar:
-            st.markdown("## ⚙️ Configuration")
-            # st.markdown("<h4>⚙️ Configuration</h4>", unsafe_allow_html=True)
+            # st.markdown("## ⚙️ Configuration")
+            st.markdown("<h4>⚙️ Configuration</h4>", unsafe_allow_html=True)
 
             mode = st.radio("Function Source", ["Predefined", "Custom"])
             func_name = st.selectbox("Function", list(predefined_funcs.keys())) if mode == "Predefined" else None
